da25ca45302799ae6143c9c9ae4184cb2aa04506
saturn1.mc <saturn1.mc@10909494-1676-11df-a4dd-1934c9c2caca>
2010-02-12 08:47:43 +0000

    Changing "writers" package name to "painters"

--- trunk/MosArt/src/gui/MosArtGUI.java
+++ trunk/MosArt/src/gui/MosArtGUI.java
@@ -3,7 +3,6 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
