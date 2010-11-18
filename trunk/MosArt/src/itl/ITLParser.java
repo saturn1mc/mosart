9cb624de334f52b9af24deab8fe7cc2ceb60dde4
saturn1.mc <saturn1.mc@10909494-1676-11df-a4dd-1934c9c2caca>
2010-11-18 12:28:33 +0000

    Give up SAX version of travis ITL parser. Using homemade one.

--- trunk/MosArt/src/itl/ITLParser.java
+++ trunk/MosArt/src/itl/ITLParser.java
@@ -1,145 +1,198 @@
 package itl;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
+public class ITLParser {
 
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
+	// States
+	private static final int STATE_INITIAL = 0;
+	private static final int STATE_HEADER = 1;
+	private static final int STATE_LIBRARY = 2;
+	private static final int STATE_TRACK = 3;
+	private static final int STATE_FIELD_CONTENT = 4;
 
-/**
- * Adapted from Travis example :
- * "Parsing the iTunes Library using a SAX parser (in Java)"
- * http://trav.is/thoughts/2010/03/30/parsing-itunes-library-using-sax-parser-java/
- * @author cmaurice2
- *
- */
-public class ITLParser extends DefaultHandler {
+	private static final char MARK_START = '<';
+	private static final char MARK_STOP = '>';
+	private static final char CLOSE_SIGN = '/';
 
-	private static final String LIBRARY_FILE_PATH = "D:\\Mes Documents\\My Music\\iTunes\\iTunes Music Library.xml";
-	private static final String KEY = "key";
 	private static final String DICT = "dict";
-	private static final String STR = "string";
+	private static final String KEY = "key";
 	private static final String INTG = "integer";
-	private static final String NAME = "Name";
-	private static final String ARTIST = "Artist";
-	private static final String ALBUM = "Album";
-	private static final String PLAY_COUNT = "Play Count";
-	private static final String PLAYLISTS = "Playlists";
-	private static final String TRACKS = "Tracks";
-	private static final String LOCATION = "Location";
+	private static final String STRG = "string";
+	private static final String DATE = "date";
+	private static final String TRUE = "true";
+	private static final String FALSE = "false";
 
-	private List<ITLSong> tracks;
-	private String stamp;
+	private BufferedReader reader;
+	private char[] currentChar;
+	private ITLSong currentSong;
 
-	private ITLSong song;
-
-	boolean tracksFound = false;
-	private String previousTag;
-	private String previousTagVal;
+	private int state;
+	private ArrayList<ITLSong> songs;
 
 	public ITLParser() {
-		tracks = new ArrayList<ITLSong>();
+		state = STATE_INITIAL;
+		songs = new ArrayList<ITLSong>();
+		currentChar = new char[1];
 	}
 
-	public void runExample() {
-		parseDocument();
-		printData();
-	}
-
-	private void parseDocument() {
+	public ArrayList<ITLSong> parseITL(String iTunesLibrary){
 		try {
-			SAXParserFactory spf = SAXParserFactory.newInstance();
-			SAXParser sp = spf.newSAXParser();
-			sp.parse(LIBRARY_FILE_PATH, this);
+			reader = new BufferedReader(new FileReader(iTunesLibrary));
+			String tag = nextTag();
+			
+			while(tag != null){
+				
+				switch (state) {
+				case STATE_INITIAL :
+					
+					if(tag.equalsIgnoreCase(openTag(DICT))){
+						state = STATE_HEADER;
+					}
+					
+					break;
 
-		} catch (SAXException se) {
-			se.printStackTrace();
-		} catch (ParserConfigurationException pce) {
-			pce.printStackTrace();
-		} catch (IOException ie) {
-			ie.printStackTrace();
-		}
-	}
-
-	private void printData() {
-		System.out.println("No of Tracks '" + tracks.size() + "'.");
-		Iterator<ITLSong> it = tracks.iterator();
-		while (it.hasNext()) {
-			ITLSong song = it.next();
-			System.out.println(song.getAlbum() + " - " + song.getName() + "-" + song.getLocation());
-		}
-	}
-
-	// Event Handlers
-	@Override
-	public void startElement(String uri, String localName, String qName,
-			Attributes attributes) throws SAXException {
-		// reset
-		stamp = "";
-
-		if (tracksFound) {
-			if (KEY.equals(previousTag) && DICT.equalsIgnoreCase(qName)) {
-				// create a new instance of employee
-				song = new ITLSong();
-				tracks.add(song);
-			}
-		} else {
-			if (KEY.equals(previousTag)
-					&& TRACKS.equalsIgnoreCase(previousTagVal)
-					&& DICT.equalsIgnoreCase(qName)) {
-				tracksFound = true; // We are now inside the Tracks dict.
-			}
-		}
-	}
-
-	public void characters(char[] ch, int start, int length)
-			throws SAXException {
-		stamp = new String(ch, start, length);
-	}
-
-	public void endElement(String uri, String localName, String qName)
-			throws SAXException {
-		if (tracksFound) {
-			if (previousTagVal.equalsIgnoreCase(NAME) && qName.equals(STR)) {
-				song.setName(stamp);
-			} else if (previousTagVal.equalsIgnoreCase(ARTIST)
-					&& qName.equals(STR)) {
-				song.setArtist(stamp);
-			} else if (previousTagVal.equalsIgnoreCase(ALBUM)
-					&& qName.equals(STR)) {
-				song.setAlbum(stamp);
-			} else if (previousTagVal.equalsIgnoreCase(PLAY_COUNT)
-					&& qName.equals(INTG)) {
-				Integer value = Integer.parseInt(stamp);
-				song.setPlayCount(value.intValue());
-			}else if (previousTagVal.equalsIgnoreCase(LOCATION)
-					&& qName.equals(STR)) {
-				song.setLocation(stamp);
+				case STATE_HEADER :
+					
+					if(tag.equalsIgnoreCase(openTag(DICT))){
+						state = STATE_LIBRARY;
+					}
+					
+					break;
+					
+				case STATE_LIBRARY :
+					
+					if(tag.equalsIgnoreCase(openTag(KEY))){
+						currentSong = new ITLSong(getTagContent());
+						songs.add(currentSong);
+					}
+					
+					if(tag.equalsIgnoreCase(openTag(DICT))){
+						state = STATE_TRACK;
+						break;
+					}
+					
+					break;
+				
+				case STATE_TRACK :
+					
+					if(tag.equalsIgnoreCase(openTag(KEY))){
+						state = STATE_FIELD_CONTENT;
+						break;
+					}
+					
+					if(tag.equalsIgnoreCase(closeTag(DICT))){
+						state = STATE_LIBRARY;
+						break;
+					}
+					
+					break;
+					
+				case STATE_FIELD_CONTENT :
+					
+					if(tag.equalsIgnoreCase(closeTag(INTG))){
+						state = STATE_TRACK;
+						break;
+					}
+					
+					if(tag.equalsIgnoreCase(closeTag(STRG))){
+						state = STATE_TRACK;
+						break;
+					}
+					
+					if(tag.equalsIgnoreCase(closeTag(DATE))){
+						state = STATE_TRACK;
+						break;
+					}
+					
+					if(tag.equalsIgnoreCase(closeTag(TRUE))){
+						state = STATE_TRACK;
+						break;
+					}
+					
+					if(tag.equalsIgnoreCase(closeTag(FALSE))){
+						state = STATE_TRACK;
+						break;
+					}
+					
+					break;
+					
+				default:
+					//Nothing
+					break;
+				}
+				
+				//Read next tag
+				tag = nextTag();
 			}
 			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return songs;
+	}
+
+	private String openTag(String tagID) {
+		return MARK_START + tagID + MARK_STOP;
+	}
+	
+	private String closeTag(String tagID) {
+		return MARK_START + CLOSE_SIGN + tagID + MARK_STOP;
+	}
+
+	private String nextTag() throws IOException {
+
+		if (reader.read(currentChar) > 0) {
+			String tag = new String();
 			
-			// Mark when we come to the end of the tracks dict.
-			if (KEY.equals(qName) && PLAYLISTS.equalsIgnoreCase(stamp)) {
-				tracksFound = false;
+			while (currentChar[0] != MARK_START) {
+				if (reader.read(currentChar) < 0) {
+					break;
+				}
+			}
+
+			tag += currentChar[0];
+
+			while (currentChar[0] != MARK_STOP) {
+				if (reader.read(currentChar) < 0) {
+					break;
+				}
+				tag += currentChar[0];
+			}
+
+			return tag;
+		}
+		
+		return null;
+	}
+
+	private String getTagContent() throws IOException {
+		String content = new String();
+
+		while (currentChar[0] != MARK_STOP) {
+
+			content += currentChar[0];
+
+			if (reader.read(currentChar) < 0) {
+				break;
 			}
 		}
 
-		// Keep track of the previous tag so we can track the context when we're
-		// at the second tag in a key, value pair.
-		previousTagVal = stamp;
-		previousTag = qName;
+		return content;
 	}
 
 	public static void main(String[] args) {
 		ITLParser spe = new ITLParser();
-		spe.runExample();
+		for (ITLSong song : spe.parseITL("D:\\iTunes Music Library.xml")) {
+			System.out.println(song);
+		}
 	}
 
 }
--- trunk/MosArt/src/itl/ITLSong.java
+++ trunk/MosArt/src/itl/ITLSong.java
@@ -1,49 +1,276 @@
 package itl;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+
 public class ITLSong {
+	
+	private final static String TRACK_ID = "Track ID";
+	private final static String NAME = "Name";
+	private final static String ARTIST = "Artist";
+	private final static String ALBUM_ARTIST = "Album Artist";
+	private final static String ALBUM = "Album";
+	private final static String GENRE = "Genre";
+	private final static String KIND = "Kind";
+	private final static String SIZE = "Size";
+	private final static String TOTAL_TIME = "Total Time";
+	private final static String DISC_NUMBER = "Disc Number";
+	private final static String DISC_COUNT = "Disc Count";
+	private final static String TRACK_NUMBER = "Track Number";
+	private final static String TRACK_COUNT = "Track Count";
+	private final static String YEAR = "Year";
+	private final static String DATE_MODIFIED = "Date Modified";
+	private final static String DATE_ADDED = "Date Added";
+	private final static String BIT_RATE = "Bit Rate";
+	private final static String SAMPLE_RATE = "Sample Rate";
+	private final static String PART_GAPLESS = "Part Of Gapless Album";
+	private final static String PLAY_COUNT = "Play Count";
+	private final static String PLAY_DATE = "Play Date";
+	private final static String PLAY_DATE_UTC = "Play Date UTC";
+	private final static String RELEASE_DATE = "Release Date";
+	private final static String PERSISTENT_ID = "Persistent ID";
+	private final static String TRACK_TYPE = "Track Type";
+	private final static String PURCHASED = "Purchased";
+	
+	private String id;
 	private String name;
 	private String artist;
+	private String albumArtist;
 	private String album;
-	private String location;
+	private String genre;
+	private String kind;
+	private int size;
+	private int totalTime;
+	private int discNumber;
+	private int discCount;
+	private int trackNumber;
+	private int trackCount;
+	private int year;
+	private Date dateModified;
+	private Date dateAdded;
+	private String bitRate;
+	private int sampleRate;
+	private boolean gapless;
 	private int playCount;
+	private Date playDate;
+	private Date dateUTC;
+	private Date releaseDate;
+	private String persistentID;
+	private String trackType;
+	private boolean purchased;
+	private String location;
+	private String fileFolderCount;
+	private String libraryFolderCount;
+	
+	public ITLSong(String id){
+		this.id = id;
+	}
+	
+	public void set(String field, String value) throws ParseException, ITLException{
+		if(field.equalsIgnoreCase(TRACK_ID)){
+			id = value;
+		}
+		else if(field.equalsIgnoreCase(NAME)){
+			name = value;
+		}
+		else if(field.equalsIgnoreCase(ARTIST)){
+			artist = value;
+		}
+		else if(field.equalsIgnoreCase(ALBUM_ARTIST)){
+			albumArtist = value;
+		}
+		else if(field.equalsIgnoreCase(ALBUM)){
+			album = value;
+		}
+		else if(field.equalsIgnoreCase(GENRE)){
+			genre = value;
+		}
+		else if(field.equalsIgnoreCase(KIND)){
+			kind = value;
+		}
+		else if(field.equalsIgnoreCase(SIZE)){
+			size = Integer.parseInt(value);
+		}
+		else if(field.equalsIgnoreCase(TOTAL_TIME)){
+			totalTime = Integer.parseInt(value);
+		}
+		else if(field.equalsIgnoreCase(DISC_NUMBER)){
+			discNumber = Integer.parseInt(value);
+		}
+		else if(field.equalsIgnoreCase(DISC_COUNT)){
+			discCount = Integer.parseInt(value);
+		}
+		else if(field.equalsIgnoreCase(TRACK_NUMBER)){
+			trackNumber = Integer.parseInt(value);
+		}
+		else if(field.equalsIgnoreCase(TRACK_COUNT)){
+			trackCount = Integer.parseInt(value);
+		}
+		else if(field.equalsIgnoreCase(YEAR)){
+			year = Integer.parseInt(value);
+		}
+		else if(field.equalsIgnoreCase(DATE_MODIFIED)){
+			dateModified = DateFormat.getDateInstance().parse(value);
+		}
+		else if(field.equalsIgnoreCase(DATE_ADDED)){
+			dateAdded = DateFormat.getDateInstance().parse(value);
+		}
+		else if(field.equalsIgnoreCase(BIT_RATE)){
+			bitRate = value;
+		}
+		else if(field.equalsIgnoreCase(SAMPLE_RATE)){
+			sampleRate = Integer.parseInt(value);
+		}
+		else if(field.equalsIgnoreCase(PART_GAPLESS)){
+			gapless = Boolean.parseBoolean(value);
+		}
+		else if(field.equalsIgnoreCase(PLAY_COUNT)){
+			playCount = Integer.parseInt(value);
+		}
+		else if(field.equalsIgnoreCase(PLAY_DATE)){
+			playDate = DateFormat.getDateInstance().parse(value);
+		}
+		else if(field.equalsIgnoreCase(PLAY_DATE_UTC)){
+			dateUTC = DateFormat.getDateInstance().parse(value);
+		}
+		else if(field.equalsIgnoreCase(RELEASE_DATE)){
+			releaseDate = DateFormat.getDateInstance().parse(value);
+		}
+		else if(field.equalsIgnoreCase(PERSISTENT_ID)){
+			persistentID = value;
+		}
+		else if(field.equalsIgnoreCase(TRACK_TYPE)){
+			trackType = value;
+		}
+		else if(field.equalsIgnoreCase(PURCHASED)){
+			purchased = Boolean.parseBoolean(value);
+		}
+		else{
+			throw new ITLException("Field '" + value + "' unknown");
+		}
+		
+		
+	}
+	
+	public String getId() {
+		return id;
+	}
 
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public String getArtist() {
 		return artist;
 	}
 
-	public void setArtist(String artistName) {
-		this.artist = artistName;
+	public String getAlbumArtist() {
+		return albumArtist;
 	}
 
 	public String getAlbum() {
 		return album;
 	}
 
-	public void setAlbum(String albumName) {
-		this.album = albumName;
+	public String getGenre() {
+		return genre;
+	}
+
+	public String getKind() {
+		return kind;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public int getTotalTime() {
+		return totalTime;
+	}
+
+	public int getDiscNumber() {
+		return discNumber;
+	}
+
+	public int getDiscCount() {
+		return discCount;
+	}
+
+	public int getTrackNumber() {
+		return trackNumber;
+	}
+
+	public int getTrackCount() {
+		return trackCount;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public Date getDateModified() {
+		return dateModified;
+	}
+
+	public Date getDateAdded() {
+		return dateAdded;
+	}
+
+	public String getBitRate() {
+		return bitRate;
+	}
+
+	public int getSampleRate() {
+		return sampleRate;
+	}
+
+	public boolean isGapless() {
+		return gapless;
 	}
 
 	public int getPlayCount() {
 		return playCount;
 	}
 
-	public void setPlayCount(int playCount) {
-		this.playCount = playCount;
+	public Date getPlayDate() {
+		return playDate;
+	}
+
+	public Date getPlayDateUTC() {
+		return dateUTC;
+	}
+
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	public String getPersistentID() {
+		return persistentID;
+	}
+
+	public String getTrackType() {
+		return trackType;
+	}
+
+	public boolean getPurchased() {
+		return purchased;
 	}
 
 	public String getLocation() {
 		return location;
 	}
 
-	public void setLocation(String location) {
-		this.location = location;
+	public String getFileFolderCount() {
+		return fileFolderCount;
+	}
+
+	public String getLibraryFolderCount() {
+		return libraryFolderCount;
+	}
+
+	@Override
+	public String toString() {
+		return this.id + " : " + this.name + " - " + this.album + " - " + this.artist;
 	}
 }
