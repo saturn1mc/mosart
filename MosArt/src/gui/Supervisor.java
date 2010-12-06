4d5cc0c4a08d06cd736edd24d0954de67e9d0791
saturn1.mc <saturn1.mc@10909494-1676-11df-a4dd-1934c9c2caca>
2010-12-06 15:05:40 +0000

    JProgressbar dump solved

--- MosArt/src/gui/Supervisor.java
+++ MosArt/src/gui/Supervisor.java
@@ -1,6 +1,9 @@
 package gui;
 
+import java.awt.Cursor;
+
 import javax.swing.JOptionPane;
+import javax.swing.SwingWorker;
 
 public class Supervisor {
 
@@ -25,76 +28,132 @@ public class Supervisor {
 
 	public synchronized void reportMainTaskFinished() {
 		if (gui != null) {
-			gui.getMainProgressBar().setIndeterminate(false);
-			gui.getMainProgressBar().setValue(
-					gui.getMainProgressBar().getMaximum());
-			gui.getMainProgressBar().setString("Done");
+			new SwingWorker<Void, Void>() {
+				@Override
+				protected Void doInBackground() throws Exception {
+					gui.getMainProgressBar().setString(null);
+					gui.getMainProgressBar().setIndeterminate(false);
+					gui.getMainProgressBar().setValue(
+							gui.getMainProgressBar().getMaximum());
+					gui.getMainProgressBar().setString("Done");
 
-			gui.getLaunchButton().setEnabled(true);
+					gui.setCursor(Cursor
+							.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+					gui.getLaunchButton().setEnabled(true);
 
-			JOptionPane.showMessageDialog(gui,
-					"Wallpaper saved to : " + gui.getTarget(), "Done",
-					JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(gui, "Wallpaper saved to : "
+							+ gui.getTarget(), "Done",
+							JOptionPane.INFORMATION_MESSAGE);
+
+					return null;
+				}
+			}.execute();
 		} else {
 			System.out.println("Wallpaper saved");
 		}
 	}
 
-	public synchronized void reportCrash(String reason) {
+	public synchronized void reportCrash(final String reason) {
 		if (gui != null) {
-			gui.getMainProgressBar().setIndeterminate(false);
-			gui.getMainProgressBar().setValue(
-					gui.getMainProgressBar().getMinimum());
-			gui.getMainProgressBar().setString("Error");
+			new SwingWorker<Void, Void>() {
+				@Override
+				protected Void doInBackground() throws Exception {
+					gui.getMainProgressBar().setString(null);
+					gui.getMainProgressBar().setIndeterminate(false);
+					gui.getMainProgressBar().setValue(
+							gui.getMainProgressBar().getMinimum());
+					gui.getMainProgressBar().setString("Error");
 
-			gui.getSubProgressBar().setIndeterminate(false);
-			gui.getSubProgressBar().setValue(
-					gui.getMainProgressBar().getMinimum());
-			gui.getSubProgressBar().setString("Error");
+					gui.getSubProgressBar().setString(null);
+					gui.getSubProgressBar().setIndeterminate(false);
+					gui.getSubProgressBar().setValue(
+							gui.getMainProgressBar().getMinimum());
+					gui.getSubProgressBar().setString("Error");
 
-			gui.getLaunchButton().setEnabled(true);
+					gui.setCursor(Cursor
+							.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+					gui.getLaunchButton().setEnabled(true);
 
-			JOptionPane.showMessageDialog(gui, reason, "Error",
-					JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(gui, reason, "Error",
+							JOptionPane.ERROR_MESSAGE);
+
+					return null;
+				}
+
+			}.execute();
 		} else {
 			System.out.println("Program crashed : " + reason);
 		}
 	}
 
-	public synchronized void reportMainTask(String task) {
+	public synchronized void reportMainTask(final String task) {
 		if (gui != null) {
-			gui.getMainProgressBar().setIndeterminate(true);
-			gui.getMainProgressBar().setString(task);
+			new SwingWorker<Void, Void>() {
+				@Override
+				protected Void doInBackground() throws Exception {
+					gui.getMainProgressBar().setString(null);
+					gui.getMainProgressBar().setIndeterminate(true);
+					gui.getMainProgressBar().setString(task);
+
+					return null;
+				}
+			}.execute();
 		} else {
 			System.out.println(task);
 		}
 	}
-	
-	public synchronized void reportMainProgress(String message, float progress) {
+
+	public synchronized void reportMainProgress(final String message,
+			final float progress) {
 		if (gui != null) {
-			gui.getMainProgressBar().setIndeterminate(false);
-			gui.getMainProgressBar().setString(message);
-			gui.getMainProgressBar().setValue((int) (100f * progress));
+			new SwingWorker<Void, Void>() {
+				@Override
+				protected Void doInBackground() throws Exception {
+					gui.getMainProgressBar().setString(null);
+					gui.getMainProgressBar().setIndeterminate(false);
+					gui.getMainProgressBar().setString(message);
+					gui.getMainProgressBar().setValue((int) (100f * progress));
+
+					return null;
+				}
+			}.execute();
 		} else {
 			System.out.println(Math.round(progress * 100f) + "% - " + message);
 		}
 	}
 
-	public synchronized void reportTask(String task) {
+	public synchronized void reportTask(final String task) {
 		if (gui != null) {
-				gui.getSubProgressBar().setIndeterminate(true);
-				gui.getSubProgressBar().setString(task);
+			new SwingWorker<Void, Void>() {
+				@Override
+				protected Void doInBackground() throws Exception {
+					gui.getSubProgressBar().setString(null);
+					gui.getSubProgressBar().setIndeterminate(true);
+					gui.getSubProgressBar().setString(task);
+
+					return null;
+				}
+			}.execute();
 		} else {
 			System.out.println(task);
 		}
 	}
-	
-	public synchronized void reportProgress(String message, float progress) {
+
+	public synchronized void reportProgress(final String message,
+			final float progress) {
 		if (gui != null) {
-			gui.getSubProgressBar().setIndeterminate(false);
-			gui.getSubProgressBar().setString(
-					Math.round(progress * 100f) + "% - " + message);
-			gui.getSubProgressBar().setValue((int) (100f * progress));
+			new SwingWorker<Void, Void>() {
+				@Override
+				protected Void doInBackground() throws Exception {
+					gui.getSubProgressBar().setString(null);
+					gui.getSubProgressBar().setIndeterminate(false);
+					gui.getSubProgressBar().setString(
+							Math.round(progress * 100f) + "% - " + message);
+					gui.getSubProgressBar().setValue((int) (100f * progress));
+
+					return null;
+				}
+			}.execute();
 		} else {
 			System.out.println(Math.round(progress * 100f) + "% - " + message);
 		}
