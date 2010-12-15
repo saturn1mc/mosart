31d9b8f8862fdcf302704847747dd88aafae8ac4
saturn1.mc <saturn1.mc@10909494-1676-11df-a4dd-1934c9c2caca>
2010-12-15 10:57:45 +0000

    Preparing new version (selecting tracks before mosaic rendering)

--- MosArt-Active/src/dependent/gui/MosArtLibraryMirror.java
+++ MosArt-Active/src/dependent/gui/MosArtLibraryMirror.java
@@ -1,5 +1,63 @@
 package dependent.gui;
 
-public class MosArtLibraryMirror {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
+
+import dependent.com.dt.iTunesController.ITTrack;
+
+public class MosArtLibraryMirror implements TreeSelectionListener {
+
+	private DefaultMutableTreeNode rootNode;
+	private JTree libraryTree;
+
+	private HashMap<String, DefaultMutableTreeNode> nodesByGenre;
+	private HashMap<String, DefaultMutableTreeNode> nodesByArtist;
+	private HashMap<String, DefaultMutableTreeNode> nodesByAlbum;
+
+	private HashMap<DefaultMutableTreeNode, ArrayList<ITTrack>> nodesTracks;
+
+	private ArrayList<ITTrack> selectedTracks;
+
+	public MosArtLibraryMirror() {
+		rootNode = new DefaultMutableTreeNode("iTunes Library");
+
+		libraryTree = new JTree(rootNode);
+		libraryTree.getSelectionModel().setSelectionMode(
+				TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION);
+		libraryTree.addTreeSelectionListener(this);
+		
+		nodesByGenre = new HashMap<String, DefaultMutableTreeNode>();
+		nodesByArtist = new HashMap<String, DefaultMutableTreeNode>();
+		nodesByAlbum = new HashMap<String, DefaultMutableTreeNode>();
+		
+		nodesTracks = new HashMap<DefaultMutableTreeNode, ArrayList<ITTrack>>();
+		
+		selectedTracks = new ArrayList<ITTrack>();
+	}
+
+	public synchronized void addTrack(ITTrack track) {
+		String genre = track.getGenre();
+		String album = track.getArtist() + track.getAlbum(); 
+	}
+	
+	public ArrayList<ITTrack> getSelectedTracks(){
+		
+		if(selectedTracks.isEmpty()){
+			selectedTracks = nodesTracks.get(rootNode);
+		}
+		
+		return selectedTracks;
+	}
+
+	@Override
+	public void valueChanged(TreeSelectionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
